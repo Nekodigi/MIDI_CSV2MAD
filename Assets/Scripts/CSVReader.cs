23d8f35cd635f40fa32f615d51a3cbd2b3c0a77f
BuildTools <unconfigured@null.spigotmc.org>
2019-10-14 10:06:15 +0900

    firstCommit

---
+++ Assets/Scripts/CSVReader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class CSVReader : MonoBehaviour
+{
+    public GameObject inNoteObj;
+    public AudioClip audioClip;
+    public float pitchOfset = 0;
+    int trackCount;
+    string path;
+    string[][] notedatas = new string[100000][];
+    float time;
+    int[] selector = new int[1000];
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = Application.dataPath + "/midi2csv.txt";
+        Debug.Log(path);
+        string[] tracks = File.ReadAllText(path).Split('/');
+        print(tracks[1]);
+        int i = 0;
+        trackCount = tracks.Length;
+        foreach (string str in tracks)
+        {
+            notedatas[i] = str.Split('\n');
+            i++;
+        }
+        print("trackCount:"+trackCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        for (int i = 0; i < trackCount-1; i++)
+        {
+            while (true)
+            {
+                if (selector[i] < notedatas.Length && notedatas[i][selector[i]] != "")
+                {
+                    string[] datas = notedatas[i][selector[i]].Split(' ');
+                    if (float.Parse(datas[0]) < time)
+                    {
+                        float pitch = Mathf.Pow(2, (float.Parse(datas[2]) - pitchOfset) / 12);
+                        GameObject noteObj = Instantiate(inNoteObj) as GameObject;
+                        noteObj.GetComponent<noteManager>().Setup(audioClip, pitch, float.Parse(datas[1]) - float.Parse(datas[0]));
+                        print("playAt:" + i + ":" + selector[i]);
+                        print("pitch:" + pitch);
+                        selector[i]++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/noteManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noteManager : MonoBehaviour
+{
+    float time = 0;
+    float timeLimit = Mathf.Infinity;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Setup(AudioClip audioClip, float pitch, float time)
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.pitch = pitch;
+        audioSource.Play();
+        timeLimit = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > timeLimit)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
